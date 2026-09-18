7475dff6c214db382ec6fe2e38cb778a9f0f68ea
Carl <monnaert.carl@gmail.com>
2026-09-18 11:14:07 +0200

    Step 21: evaluate_regression

--- model.py
+++ model.py
@@ -213,8 +213,11 @@ def r_squared(y_true, y_pred):
     SStot = sum( (y_true - y_true.mean() ) ** 2 )
     return 1 - SSres/SStot if SStot != 0 else float('nan')
 
-# Step 21 - evaluate_regression (not yet solved)
-# TODO: implement
+# Step 21 - evaluate_regression
+def evaluate_regression(y_true, y_pred):
+    return { 'mae' : mean_absolute_error(y_true, y_pred),
+             'rmse' : root_mean_squared_error(y_true, y_pred),
+             'r2' : r_squared(y_true, y_pred) }
 
 # Step 22 - learning_curve_data (not yet solved)
 # TODO: implement
